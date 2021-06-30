06de132f9d064216ecb6931b7d62aa49f66a160c
Jonman <Jonman@yandex.ru>
2021-06-30 16:12:29 +0700

    new lesson from book

--- C/latlon.c
+++ C/latlon.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-void go_south_east (int lat, int lon)
+void go_south_east (int *lat, int *lon)
 {
-	lat--;
-	lon++;
+	(*lat)--; // *lat--; Почему не работает?
+	*(lon)++; // *lon++; Почему не работает?
 }
 
 int main()
 {
 	int latitude = 32;
 	int longitude = -64;
-	go_south_east(latitude, longitude);
+	go_south_east(&latitude, &longitude);
 	printf("Стоп! Теперь наши координаты: [%i, %i]\n", latitude, longitude);
 	return 0;
 }
